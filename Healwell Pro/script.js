9542f5cc1bc7a97275e6b7f7f85e446145389874
FAISAL FAYYAZ <faisalfayyaz500@gmail.com>
2025-11-06 11:25:39 +0500

    Add files via upload

---
+++ Healwell Pro/script.js
@@ -0,0 +1,13 @@
+function sendMail(){
+    let params = {
+    name : document.getElementById("name").value,
+    email : document.getElementById("email").value,
+    subject : document.getElementById("subject").value,
+    message : document.getElementById("message").value,
+
+
+
+    }
+
+    emailjs.sendMail("service_cz4n6oo","template_cbzi3vk",params).then(alert(Email Sent!!))
+}
